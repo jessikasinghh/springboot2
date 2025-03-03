b69e2d1ff7223563e88e44b7f2f59773a662c6d3
jessikasinghh <128406656+jessikasinghh@users.noreply.github.com>
2025-03-03 15:16:37 +0530

    UC2 commited

--- springboot2/src/main/java/com/example/springboot2/Springboot2Application.java
+++ springboot2/src/main/java/com/example/springboot2/Springboot2Application.java
@@ -1,18 +1,45 @@
 package com.example.springboot2;
 
-//UC1
+import com.example.springboot2.DemoBean;
+//UC2
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("com.springlog.springlogmessage.components")  // Ensures DemoBean is found
 public class Springboot2Application {
+	public static final Logger logger = LoggerFactory.getLogger(Springboot2Application.class);
+
 	public static void main(String[] args) {
+		logger.debug("Welcome to Spring Concept Demo");
+
 		ApplicationContext context = SpringApplication.run(Springboot2Application.class, args);
 
-		// Get DemoBean and use it
-		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println(demoBean.getMessage());  // Output: Hello from DemoBean!
+		logger.debug("Checking Context: {}", context.getBean(DemoBean.class));
+		logger.debug("\n*** Example Using @Autowire annotation on property ***");
 	}
 }
 
+
+
+//UC1
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ApplicationContext;
+//
+//@SpringBootApplication
+//public class Springboot2Application {
+//	public static void main(String[] args) {
+//		ApplicationContext context = SpringApplication.run(Springboot2Application.class, args);
+//
+//		// Get DemoBean and use it
+//		DemoBean demoBean = context.getBean(DemoBean.class);
+//		System.out.println(demoBean.getMessage());  // Output: Hello from DemoBean!
+//	}
+//}
+//
